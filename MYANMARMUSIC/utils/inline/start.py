ccf2c66c5d2a3eb27046a1b85a5e686fe586ce91
powerfulbot1999 <musicpowerful22@gmail.com>
2026-03-08 20:45:14 +0630

    MYANMARMUSIC

--- MYANMARMUSIC/utils/inline/start.py
+++ MYANMARMUSIC/utils/inline/start.py
@@ -48,18 +48,18 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(
-                text=_["S_B_11"],
-                callback_data="about_page"
+                text="𝑷𝒐𝒘𝒆𝒓𝑭𝒖𝒍 𝑪𝒉𝒂𝒕",
+                url="https://t.me/powerful_youth2025"
             ),
             InlineKeyboardButton(
-                text=_["S_B_12"],
-                callback_data="owner_page"
+                text="𝙏𝙔𝙋𝙄𝘾ᴾᵂᶠ 𝑺𝒉𝒐𝒑",
+                url="https://t.me/typicgamestore3"
             )
         ],
         [
             InlineKeyboardButton(
-                text=_["E_X_1"],
-                callback_data="fork_repo"
+                text="𝑷𝒐𝒘𝒆𝒓𝒇𝒖𝒍 𝑴𝒖𝒔𝒊𝒄🎧",
+                url="https://t.me/just_now2025"
             ),
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
         ],
